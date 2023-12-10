6984d3380678abdb780ceac3cdff2f39b55d0b53
Marko Topolnik <marko.topolnik@gmail.com>
2023-12-10 19:24:51 +0100

    Day 10 Part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 use std::iter::repeat;
+use std::ops::Range;
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 enum Direction {
@@ -9,6 +10,23 @@ enum Direction {
     Down,
 }
 use Direction::*;
+
+#[derive(Copy, Clone, Debug)]
+struct Tile {
+    symbol: u8,
+    on_path: bool,
+    inside: bool,
+}
+
+impl From<u8> for Tile {
+    fn from(symbol: u8) -> Self {
+        Tile {
+            symbol,
+            on_path: false,
+            inside: false,
+        }
+    }
+}
 
 fn main() {
     let input_lines: Vec<String> = read_to_string("input/day10.txt")
@@ -25,53 +43,186 @@ fn main() {
     }
     lines.push(empty_line);
 
-    let maze: Vec<Vec<u8>> = lines.into_iter().map(|s| s.as_bytes().to_owned()).collect();
+    let mut maze: Vec<Vec<Tile>> = lines
+        .into_iter()
+        .map(|s| s.bytes().map(|b| b.into()).collect())
+        .collect();
 
-    let (mut y, mut x) = find_start(&maze);
-    let mut direction;
-
-    direction = {
-        if [b'|', b'7', b'F'].contains(&maze[y - 1][x]) {
+    let (start_y, start_x) = find_start(&maze);
+    maze[start_y][start_x].on_path = true;
+    let start_direction = {
+        if [b'|', b'7', b'F'].contains(&maze[start_y - 1][start_x].symbol) {
             Up
-        } else if [b'|', b'J', b'L'].contains(&maze[y + 1][x]) {
-            Down
-        } else if [b'-', b'L', b'F'].contains(&maze[y][x - 1]) {
+        } else if [b'-', b'L', b'F'].contains(&maze[start_y][start_x - 1].symbol) {
             Left
-        } else if [b'-', b'J', b'7'].contains(&maze[y][x + 1]) {
+        } else if [b'-', b'J', b'7'].contains(&maze[start_y][start_x + 1].symbol) {
             Right
+        } else if [b'|', b'J', b'L'].contains(&maze[start_y + 1][start_x].symbol) {
+            Down
         } else {
             panic!("dead end");
         }
     };
-    (y, x) = follow_direction(y, x, direction);
 
+    let (mut y, mut x) = (start_y, start_x);
     let mut steps = 1;
+    let mut net_rotation = 0;
+    let mut direction = start_direction;
     loop {
-        let on_ch = maze[y][x];
-        if on_ch == b'S' {
+        (y, x) = follow_direction(y, x, direction);
+        let tile = &mut maze[y][x];
+        if tile.symbol == b'S' {
             break;
         }
+        tile.on_path = true;
         steps += 1;
-        direction = {
-            match (on_ch, direction) {
-                (b'|', Up) => Up,
-                (b'|', Down) => Down,
-                (b'-', Left) => Left,
-                (b'-', Right) => Right,
-                (b'7', Up) => Left,
-                (b'7', Right) => Down,
-                (b'F', Up) => Right,
-                (b'F', Left) => Down,
-                (b'J', Down) => Left,
-                (b'J', Right) => Up,
-                (b'L', Down) => Right,
-                (b'L', Left) => Up,
-                _ => panic!("{}, {direction:?}", on_ch as char),
-            }
-        };
-        (y, x) = follow_direction(y, x, direction);
+
+        let prev_direction = direction;
+        direction = new_direction(tile.symbol, direction);
+        net_rotation += get_rotation(prev_direction, direction);
     }
-    println!("Farthest distance: {}", steps / 2);
+    println!(
+        "Farthest distance: {}, net rotation: {net_rotation}",
+        steps / 2
+    );
+
+    (y, x) = (start_y, start_x);
+    direction = start_direction;
+    loop {
+        (y, x) = follow_direction(y, x, direction);
+        let on_symbol = maze[y][x].symbol;
+        if on_symbol == b'S' {
+            break;
+        }
+        match (on_symbol, direction) {
+            (b'|', Up) => mark_left(y, x, &mut maze),
+            (b'|', Down) => mark_right(y, x, &mut maze),
+            (b'-', Left) => mark_down(y, x, &mut maze),
+            (b'-', Right) => mark_up(y, x, &mut maze),
+            (b'7', Up) => {
+                mark_left(y, x, &mut maze);
+                mark_down(y, x, &mut maze);
+            }
+            (b'7', Right) => {
+                mark_up(y, x, &mut maze);
+                mark_right(y, x, &mut maze);
+            }
+            (b'F', Up) => {
+                mark_left(y, x, &mut maze);
+                mark_up(y, x, &mut maze);
+            }
+            (b'F', Left) => {
+                mark_down(y, x, &mut maze);
+                mark_right(y, x, &mut maze);
+            }
+            (b'J', Down) => {
+                mark_right(y, x, &mut maze);
+                mark_down(y, x, &mut maze);
+            }
+            (b'J', Right) => {
+                mark_up(y, x, &mut maze);
+                mark_left(y, x, &mut maze);
+            }
+            (b'L', Down) => {
+                mark_right(y, x, &mut maze);
+                mark_up(y, x, &mut maze);
+            }
+            (b'L', Left) => {
+                mark_down(y, x, &mut maze);
+                mark_left(y, x, &mut maze);
+            }
+            _ => {}
+        }
+        direction = new_direction(on_symbol, direction);
+    }
+
+    for row in maze.iter() {
+        println!(
+            "{}",
+            row.iter()
+                .map(|&tile| {
+                    if tile.on_path {
+                        'o'
+                    } else if tile.inside {
+                        'i'
+                    } else {
+                        '.'
+                    }
+                })
+                .collect::<String>()
+        );
+    }
+    let inner_tile_count = maze
+        .into_iter()
+        .flat_map(|row| row.into_iter())
+        .filter(|tile| tile.inside)
+        .count();
+    println!("Inner tile count: {inner_tile_count}");
+}
+
+fn mark_horizontal(y: usize, x_range: Range<usize>, maze: &mut [Vec<Tile>]) {
+    for inner_x in x_range {
+        let inner_tile = &mut maze[y][inner_x];
+        if inner_tile.on_path {
+            break;
+        }
+        inner_tile.inside = true;
+    }
+}
+
+fn mark_vertical(y_range: Range<usize>, x: usize, maze: &mut [Vec<Tile>]) {
+    for inner_y in y_range {
+        let inner_tile = &mut maze[inner_y][x];
+        if inner_tile.on_path {
+            break;
+        }
+        inner_tile.inside = true;
+    }
+}
+
+fn mark_left(y: usize, x: usize, maze: &mut [Vec<Tile>]) {
+    mark_horizontal(y, x - 1..0, maze);
+}
+
+fn mark_right(y: usize, x: usize, maze: &mut [Vec<Tile>]) {
+    mark_horizontal(y, x + 1..maze[y].len() - 1, maze);
+}
+
+fn mark_down(y: usize, x: usize, maze: &mut [Vec<Tile>]) {
+    mark_vertical(y + 1..maze.len() - 1, x, maze);
+}
+
+fn mark_up(y: usize, x: usize, maze: &mut [Vec<Tile>]) {
+    mark_vertical(y - 1..0, x, maze);
+}
+
+fn get_rotation(prev_direction: Direction, new_direction: Direction) -> i8 {
+    match (prev_direction, new_direction) {
+        (prev, new) if prev == new => 0,
+        (Up, Left) => 1,
+        (Left, Down) => 1,
+        (Down, Right) => 1,
+        (Right, Up) => 1,
+        _ => -1,
+    }
+}
+
+fn new_direction(symbol: u8, direction: Direction) -> Direction {
+    match (symbol, direction) {
+        (b'|', Up) => Up,
+        (b'|', Down) => Down,
+        (b'-', Left) => Left,
+        (b'-', Right) => Right,
+        (b'7', Up) => Left,
+        (b'7', Right) => Down,
+        (b'F', Up) => Right,
+        (b'F', Left) => Down,
+        (b'J', Down) => Left,
+        (b'J', Right) => Up,
+        (b'L', Down) => Right,
+        (b'L', Left) => Up,
+        _ => panic!("{}, {direction:?}", symbol as char),
+    }
 }
 
 fn follow_direction(y: usize, x: usize, direction: Direction) -> (usize, usize) {
@@ -83,13 +234,13 @@ fn follow_direction(y: usize, x: usize, direction: Direction) -> (usize, usize) 
     }
 }
 
-fn find_start(maze: &[Vec<u8>]) -> (usize, usize) {
+fn find_start(maze: &[Vec<Tile>]) -> (usize, usize) {
     maze.iter()
         .enumerate()
         .map(|(y, row)| {
             row.iter()
                 .enumerate()
-                .find(|(_, &ch)| ch == b'S')
+                .find(|(_, &tile)| tile.symbol == b'S')
                 .map(|(x, _)| (y, x))
         })
         .filter_map(|option| option)
