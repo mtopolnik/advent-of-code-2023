b64e40dc9b9486c8a245c5c01d8f609994d19ee9
Marko Topolnik <marko.topolnik@gmail.com>
2023-12-17 10:07:48 +0100

    Improve Day 16

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -7,7 +7,7 @@ struct Tile {
 }
 
 #[derive(Clone, Copy)]
-struct BeamState {
+struct Photon {
     y: usize,
     x: usize,
     dir_y: isize,
@@ -21,23 +21,17 @@ fn main() {
         .map(|line| line.bytes().map(|b| b.into()).collect())
         .collect();
     let energized_count =
-        count_energized_tiles(&mut grid, BeamState { y: 0, x: 0, dir_y: 0, dir_x: 1 });
+        count_energized_tiles(&mut grid, Photon { y: 0, x: 0, dir_y: 0, dir_x: 1 });
     println!("Part 1: {energized_count}"); // 8021
     let size = grid.len();
     let max_energized_count = (0..size)
         .into_iter()
         .flat_map(|i| {
             [
-                count_energized_tiles(&mut grid, BeamState { y: i, x: 0, dir_y: 0, dir_x: 1 }),
-                count_energized_tiles(
-                    &mut grid,
-                    BeamState { y: i, x: size - 1, dir_y: 0, dir_x: -1 },
-                ),
-                count_energized_tiles(&mut grid, BeamState { y: 0, x: i, dir_y: 1, dir_x: 0 }),
-                count_energized_tiles(
-                    &mut grid,
-                    BeamState { y: size - 1, x: i, dir_y: -1, dir_x: 0 },
-                ),
+                count_energized_tiles(&mut grid, Photon { y: i, x: 0, dir_y: 0, dir_x: 1 }),
+                count_energized_tiles(&mut grid, Photon { y: i, x: size - 1, dir_y: 0, dir_x: -1 }),
+                count_energized_tiles(&mut grid, Photon { y: 0, x: i, dir_y: 1, dir_x: 0 }),
+                count_energized_tiles(&mut grid, Photon { y: size - 1, x: i, dir_y: -1, dir_x: 0 }),
             ]
             .into_iter()
         })
@@ -46,18 +40,18 @@ fn main() {
     println!("Part 2: {max_energized_count}"); // 8216
 }
 
-fn count_energized_tiles(grid: &mut [Vec<Tile>], init_state: BeamState) -> usize {
+fn count_energized_tiles(grid: &mut [Vec<Tile>], init_state: Photon) -> usize {
     let size = grid.len() as isize;
-    let mut todo_list: Vec<BeamState> = vec![init_state];
-    loop {
-        let Some(BeamState { mut y, mut x, mut dir_y, mut dir_x }) = todo_list.pop() else {
-            break;
-        };
-        loop {
-            // debug_print(grid, y, x, dir_y, dir_x);
+    let mut photons: Vec<Photon> = vec![init_state];
+    while !photons.is_empty() {
+        // debug_print(grid);
+        let mut i = 0;
+        while i < photons.len() {
+            let Photon { y, x, mut dir_y, mut dir_x } = photons[i];
             let tile = &mut grid[y][x];
             let did_visit = tile.did_visit_direction_mut(dir_y, dir_x);
             if *did_visit {
+                photons.remove(i);
                 break;
             }
             *did_visit = true;
@@ -66,23 +60,26 @@ fn count_energized_tiles(grid: &mut [Vec<Tile>], init_state: BeamState) -> usize
                 b'/' => (dir_y, dir_x) = (-dir_x, -dir_y),
                 b'-' => {
                     if dir_x == 0 {
-                        add_todo_if_valid(&mut todo_list, y, x, dir_x, dir_y, size);
+                        photons.push(Photon { y, x, dir_y: dir_x, dir_x: dir_y });
                         (dir_y, dir_x) = (-dir_x, -dir_y)
                     }
                 }
                 b'|' => {
                     if dir_y == 0 {
-                        add_todo_if_valid(&mut todo_list, y, x, dir_x, dir_y, size);
+                        photons.push(Photon { y, x, dir_y: dir_x, dir_x: dir_y });
                         (dir_y, dir_x) = (-dir_x, -dir_y)
                     }
                 }
                 b'.' => {}
                 _ => panic!("Invalid tile symbol"),
             }
-            let Some((new_y, new_x)) = follow_direction(y, dir_y, x, dir_x, size) else {
-                break;
-            };
-            (y, x) = (new_y, new_x);
+            let (y, x) = (y as isize + dir_y, x as isize + dir_x);
+            if y < 0 || x < 0 || y >= size || x >= size {
+                photons.remove(i);
+            } else {
+                photons[i] = Photon { y: y as usize, x: x as usize, dir_y, dir_x };
+                i += 1;
+            }
         }
     }
     let mut energized_count = 0;
@@ -97,31 +94,6 @@ fn count_energized_tiles(grid: &mut [Vec<Tile>], init_state: BeamState) -> usize
     energized_count
 }
 
-fn add_todo_if_valid(
-    todo_list: &mut Vec<BeamState>,
-    prev_y: usize,
-    prev_x: usize,
-    dir_y: isize,
-    dir_x: isize,
-    width: isize,
-) {
-    if let Some((y, x)) = follow_direction(prev_y, dir_y, prev_x, dir_x, width) {
-        todo_list.push(BeamState { y, x, dir_x, dir_y });
-    };
-}
-
-fn follow_direction(
-    y: usize,
-    dir_y: isize,
-    x: usize,
-    dir_x: isize,
-    width: isize,
-) -> Option<(usize, usize)> {
-    let (new_y, new_x) = (y as isize + dir_y, x as isize + dir_x);
-    (new_y >= 0 && new_x >= 0 && new_y < width && new_x < width)
-        .then_some((new_y as usize, new_x as usize))
-}
-
 impl Tile {
     fn did_visit_direction_mut<'a, 'b: 'a>(
         &'b mut self,
@@ -132,21 +104,12 @@ impl Tile {
     }
 }
 
-fn debug_print(grid: &[Vec<Tile>], y: usize, x: usize, dir_y: isize, dir_x: isize) {
-    let dir_symbol = match (dir_y, dir_x) {
-        (0, 1) => '>',
-        (0, -1) => '<',
-        (-1, 0) => '^',
-        (1, 0) => 'v',
-        _ => panic!("Invalid direction"),
-    };
-    for (cur_y, row) in grid.iter().enumerate() {
-        for (cur_x, tile) in row.iter().enumerate() {
+fn debug_print(grid: &[Vec<Tile>]) {
+    for row in grid.iter() {
+        for tile in row.iter() {
             print!(
                 "{}",
-                if (cur_y, cur_x) == (y, x) {
-                    dir_symbol
-                } else if tile.symbol != b'.' {
+                if tile.symbol != b'.' {
                     tile.symbol as char
                 } else {
                     match tile.visited_directions {
@@ -163,7 +126,7 @@ fn debug_print(grid: &[Vec<Tile>], y: usize, x: usize, dir_y: isize, dir_x: isiz
         println!();
     }
     println!();
-    sleep(Duration::new(0, 10_000_000));
+    sleep(Duration::new(0, 4_000_000));
 }
 
 impl From<u8> for Tile {
