79ff3e475b564a3dd55347d13b82eecf0a7deda8
Marko Topolnik <marko.topolnik@gmail.com>
2023-12-18 19:10:41 +0100

    Day 18 Part 2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,75 +1,97 @@
-use std::{fs::read_to_string, iter::repeat, thread::sleep, time::Duration};
+use std::{collections::VecDeque, fs::read_to_string};
 
 fn main() {
     let input = &read_to_string("input/day18.txt").unwrap();
-    let instructions: Vec<(Direction, usize)> = input
-        .lines()
-        .map(|line| {
-            let [dir_str, count_str, ..] = line.split(" ").collect::<Vec<_>>()[..] else {
-                panic!("Parse error");
-            };
-            (Direction::parse(dir_str), str::parse(count_str).unwrap())
-        })
-        .collect();
-    let (ground, start_y, start_x) = build_trench(&instructions);
-    let ground = dig_out_interior(ground);
-    debug_print(&ground, start_y, start_x);
-    let dugout_block_count = ground
-        .into_iter()
-        .flat_map(|row| row.into_iter())
-        .filter(|&block| block == b'#')
-        .count();
-    println!("Part 1: {dugout_block_count}"); // 53844
+    let (instructions_1, instructions_2): (Vec<(Direction, usize)>, Vec<(Direction, usize)>) =
+        input
+            .lines()
+            .map(|line| {
+                let [dir_str, count_str, code] = line.split(" ").collect::<Vec<_>>()[..] else {
+                    panic!("Parse error");
+                };
+                (
+                    (
+                        Direction::parse_part1(dir_str),
+                        str::parse(count_str).unwrap(),
+                    ),
+                    (
+                        Direction::parse_part2(code.chars().nth(7).unwrap()),
+                        usize::from_str_radix(&code[2..7], 16).unwrap(),
+                    ),
+                )
+            })
+            .unzip();
+    let ground = build_trench(&instructions_1);
+    let count_part1 = dugout_block_count(ground);
+    println!("Part 1: {count_part1}\n\n");
+
+    let ground = build_trench(&instructions_2);
+    let count_part2 = dugout_block_count(ground);
+    println!("Part 2: {count_part2}"); // 42708339569950
 }
 
-fn build_trench(instructions: &[(Direction, usize)]) -> (Vec<Vec<u8>>, isize, isize) {
-    let mut ground = Vec::<Vec<u8>>::new();
-    ground.push("...".as_bytes().to_vec());
-    ground.push(".#.".as_bytes().to_vec());
-    ground.push("...".as_bytes().to_vec());
-    let (mut start_y, mut start_x) = (1_isize, 1_isize);
-    let (mut y, mut x) = (start_y, start_x);
+fn build_trench(instructions: &[(Direction, usize)]) -> VecDeque<Vec<u32>> {
+    let start_x = 2_000_000_u32;
+    let mut ground: VecDeque<Vec<u32>> = VecDeque::new();
+    ground.push_back(vec![]);
+    ground.push_back(vec![start_x]);
+    ground.push_back(vec![]);
+    let (mut y, mut x) = (1_isize, start_x as isize);
     for &(dir, count) in instructions {
         let (delta_y, delta_x) = dir.delta();
         for _ in 0..count {
             (y, x) = (y + delta_y, x + delta_x);
-            ground[y as usize][x as usize] = b'#';
+            if x == 0 {
+                panic!("Reached left edge of ground!");
+            }
+            ground[y as usize].push(x as u32);
             if y == 0 {
-                ground.insert(0, repeat(b'.').take(ground[0].len()).collect());
+                ground.push_front(vec![]);
                 y = 1;
-                start_y += 1;
             } else if y == ground.len() as isize - 1 {
-                ground.push(repeat(b'.').take(ground[0].len()).collect());
-            } else if x == 0 {
-                for i in 0..ground.len() {
-                    ground[i].insert(0, b'.');
-                }
-                x = 1;
-                start_x += 1;
-            } else if x == ground[0].len() as isize - 1 {
-                for i in 0..ground.len() {
-                    ground[i].push(b'.');
-                }
+                ground.push_back(vec![]);
             }
         }
     }
-    (ground, start_y, start_x)
+    for row in ground.iter_mut() {
+        row.sort();
+    }
+    ground
 }
 
-fn dig_out_interior(mut ground: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-    let trench_map = ground.clone();
-    for y in 1..trench_map.len() - 1 {
+fn dugout_block_count(ground: VecDeque<Vec<u32>>) -> usize {
+    let mut dugout_count = 0_usize;
+    let print_skip = ground.len() / 20;
+    for y in 1..ground.len() - 1 {
+        if y % print_skip == 0 {
+            println!("{:.1}% complete", 100.0 * y as f32 / ground.len() as f32);
+        }
+        let row = &ground[y];
+        let mut prev_trench_x = -1_i32;
         let mut inside = false;
-        let mut expected_edge = Some((b'#', b'#', b'#'));
-        expected_edge = None;
-        for x in 1..trench_map[0].len() - 1 {
-            let edge = (trench_map[y - 1][x], trench_map[y][x], trench_map[y + 1][x]);
-            match edge {
-                (_, b'.', _) => {
-                    if inside {
-                        ground[y][x] = b'#';
-                    }
+        let mut expected_edge: Option<(u8, u8, u8)> = None;
+        for trench_x in row {
+            if *trench_x as i32 <= prev_trench_x {
+                println!("{trench_x} <= {prev_trench_x}");
+                continue;
+            }
+            dugout_count += 1;
+            if inside {
+                if prev_trench_x < 0 {
+                    println!("prev_trench_x < 0: {prev_trench_x}");
                 }
+                dugout_count += (trench_x - (prev_trench_x as u32) - 1) as usize;
+            }
+            let symbol_above = match ground[y - 1].contains(trench_x) {
+                true => b'#',
+                false => b'.',
+            };
+            let symbol_below = match ground[y + 1].contains(trench_x) {
+                true => b'#',
+                false => b'.',
+            };
+            let edge = (symbol_above, b'#', symbol_below);
+            match edge {
                 (b'#', b'#', b'#') => inside = !inside,
                 (b'.', b'#', b'.') => {}
                 (_, b'#', _) => {
@@ -88,9 +110,10 @@ fn dig_out_interior(mut ground: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
                 }
                 _ => panic!(),
             }
+            prev_trench_x = *trench_x as i32;
         }
     }
-    ground
+    dugout_count
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
@@ -126,40 +149,32 @@ impl From<Direction> for u8 {
 }
 
 impl Direction {
-    fn parse(dir_str: &str) -> Self {
+    fn parse_part1(dir_str: &str) -> Self {
         match dir_str {
-            "U" => Up,
             "R" => Right,
             "D" => Down,
             "L" => Left,
+            "U" => Up,
             _ => panic!("Invalid instruction!"),
+        }
+    }
+
+    fn parse_part2(dir_ch: char) -> Self {
+        match dir_ch {
+            '0' => Right,
+            '1' => Down,
+            '2' => Left,
+            '3' => Up,
+            _ => panic!("{dir_ch}"),
         }
     }
 
     fn delta(&self) -> (isize, isize) {
         match self {
-            Up => (-1_isize, 0_isize),
             Right => (0, 1),
             Down => (1, 0),
             Left => (0, -1),
+            Up => (-1_isize, 0_isize),
         }
     }
-}
-
-fn debug_print(ground: &[Vec<u8>], start_y: isize, start_x: isize) {
-    for (y, row) in ground.iter().enumerate() {
-        for (x, &symbol) in row.iter().enumerate() {
-            print!(
-                "{}",
-                if y as isize == start_y && x as isize == start_x {
-                    'X'
-                } else {
-                    symbol as char
-                }
-            );
-        }
-        println!();
-    }
-    println!();
-    sleep(Duration::new(0, 100_000_000));
 }
