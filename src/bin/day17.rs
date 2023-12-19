226f3a2176e7ff39fb6af7626ecd507ec0d17b84
Marko Topolnik <marko.topolnik@gmail.com>
2023-12-19 07:35:19 +0100

    Day 17 Part 2

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -32,17 +32,18 @@ fn main() {
         .lines()
         .map(|line| line.bytes().map(|b| b - b'0').collect())
         .collect();
-    println!("Part 1: {}", least_heat_loss(city)); // 1263
+    println!("Part 1: {}", least_heat_loss(&city, part1_neighbor_filter)); // 1263
+    println!("Part 2: {}", least_heat_loss(&city, part2_neighbor_filter)); // 1411
 }
 
-fn least_heat_loss(city: Vec<Vec<u8>>) -> u32 {
+fn least_heat_loss(city: &[Vec<u8>], neighbor_filter_fn: impl Fn(Turn, u8) -> bool) -> u32 {
     let height = city.len() as u16;
     let width = city[0].len() as u16;
     let mut unvisited: PriorityQueue<DijkstraNode, u32> = PriorityQueue::new();
     for y in 0..height {
         for x in 0..width {
             for direction in [North, East, South, West] {
-                for straight_steps in [1, 2, 3] {
+                for straight_steps in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] {
                     unvisited.push(
                         DijkstraNode {
                             y: y as u16,
@@ -61,18 +62,22 @@ fn least_heat_loss(city: Vec<Vec<u8>>) -> u32 {
         u32::MAX,
     );
     loop {
-        let Some((dijkstra_node, priority)) = unvisited.pop() else {
+        let Some((visiting, priority)) = unvisited.pop() else {
             panic!("No route to the factory!");
         };
-        let DijkstraNode { y, x, direction, straight_steps } = dijkstra_node;
+        let DijkstraNode { y, x, direction, straight_steps } = visiting;
         if priority == 0 {
             panic!("No route to the factory!")
         }
         let heat_loss = u32::MAX - priority;
-        if y == height - 1 && x == width - 1 {
+        let neighbors = neighbors(
+            &city,
+            DijkstraNode { y, x, direction, straight_steps },
+            &neighbor_filter_fn,
+        );
+        if y == height - 1 && x == width - 1 && !neighbors.is_empty() {
             return heat_loss;
         }
-        let neighbors = neighbors(&city, DijkstraNode { y, x, direction, straight_steps });
         for neighbor in neighbors {
             let DijkstraNode { y: neighbor_y, x: neighbor_x, .. } = neighbor;
             let Some((_, &priority)) = unvisited.get(&neighbor) else {
@@ -88,14 +93,41 @@ fn least_heat_loss(city: Vec<Vec<u8>>) -> u32 {
     }
 }
 
-fn neighbors(city: &[Vec<u8>], step: DijkstraNode) -> Vec<DijkstraNode> {
-    let DijkstraNode { y, x, direction, straight_steps } = step;
+fn neighbors(
+    city: &[Vec<u8>],
+    node: DijkstraNode,
+    neighbor_filter_fn: &impl Fn(Turn, u8) -> bool,
+) -> Vec<DijkstraNode> {
+    let DijkstraNode { y, x, direction, straight_steps } = node;
     let height = city.len() as i16;
     let width = city[0].len() as i16;
     let y = y as i16;
     let x = x as i16;
-    let Some(incoming_direction) = direction else {
-        return vec![
+    if let Some(incoming_direction) = direction {
+        [Straight, Left, Right]
+            .into_iter()
+            .filter_map(|turn| {
+                let outgoing_direction = incoming_direction.after_turn(turn);
+                let (y, x) = follow_direction(outgoing_direction, y, x);
+                (y >= 0
+                    && x >= 0
+                    && y < height
+                    && x < width
+                    && neighbor_filter_fn(turn, straight_steps))
+                .then_some(DijkstraNode {
+                    y: y as u16,
+                    x: x as u16,
+                    direction: Some(outgoing_direction),
+                    straight_steps: if turn == Straight {
+                        straight_steps as i8 + 1
+                    } else {
+                        1
+                    } as u8,
+                })
+            })
+            .collect()
+    } else {
+        vec![
             DijkstraNode {
                 y: 0,
                 x: 1,
@@ -108,28 +140,31 @@ fn neighbors(city: &[Vec<u8>], step: DijkstraNode) -> Vec<DijkstraNode> {
                 direction: Some(South),
                 straight_steps: 1,
             },
-        ];
-    };
-    [Straight, Left, Right]
-        .into_iter()
-        .filter_map(|turn| {
-            let straight_steps = if turn == Straight {
-                straight_steps as i8 + 1
-            } else {
-                1
-            };
-            let outgoing_direction = incoming_direction.after_turn(turn);
-            let (y, x) = follow_direction(outgoing_direction, y, x);
-            (straight_steps <= 3 && y >= 0 && x >= 0 && y < height && x < width).then_some(
-                DijkstraNode {
-                    y: y as u16,
-                    x: x as u16,
-                    direction: Some(outgoing_direction),
-                    straight_steps: straight_steps as u8,
-                },
-            )
-        })
-        .collect()
+        ]
+    }
+}
+
+fn part1_neighbor_filter(turn: Turn, straight_steps: u8) -> bool {
+    turn != Straight || straight_steps < 3
+}
+
+fn part2_neighbor_filter(turn: Turn, straight_steps: u8) -> bool {
+    if turn == Straight {
+        straight_steps < 10
+    } else {
+        straight_steps >= 4
+    }
+}
+
+impl Direction {
+    fn after_turn(&self, turn: Turn) -> Direction {
+        let delta = 4 + match turn {
+            Left => -1,
+            Straight => 0,
+            Right => 1,
+        };
+        (((u8::from(*self) + delta as u8) % 4) as u8).into()
+    }
 }
 
 fn follow_direction(direction: Direction, y: i16, x: i16) -> (i16, i16) {
@@ -160,23 +195,6 @@ impl From<Direction> for u8 {
             East => 1,
             South => 2,
             West => 3,
-        }
-    }
-}
-
-impl Direction {
-    fn after_turn(&self, turn: Turn) -> Direction {
-        let delta = i8::from(turn) + 4;
-        (((u8::from(*self) + delta as u8) % 4) as u8).into()
-    }
-}
-
-impl From<Turn> for i8 {
-    fn from(value: Turn) -> Self {
-        match value {
-            Left => -1,
-            Straight => 0,
-            Right => 1,
         }
     }
 }
